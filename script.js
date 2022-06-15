ed6442ed321f42136d71823d732e6c3378936289
Simon Gowing <darkdefender320@gmail.com>
2022-06-15 21:15:00 +0100

    Got the game to play 1 round by clicking a button but the computer selection doesn't change at the moment to a random selection and the scores go up in value when it doesn't need to.

--- script.js
+++ script.js
@@ -3,39 +3,25 @@ let playerScore = 0;
 //Variable to keep track of the computer score in the game
 let computerScore = 0;
 
-function playerPlay(){
-    //Prompt user for their selection and store in a variable playerSelect
-    let playerSelect = prompt('Rock, Paper or Scissors?');
-    //if the user didn't input anything this will alert the user and ask them again
-    while(playerSelect == null){
-        alert('Please enter Rock, Paper or Scissors');
-        playerSelect = prompt('Rock, Paper or Scissors?');
-    }
-    //Convert everything but the first letter the user inputted to lowercase
-    let lowercaseRestOfWord = playerSelect.slice(1).toLowerCase();
-    //Convert the first letter the user inputted to uppercase
-    let capitalLetter = playerSelect[0].toUpperCase();
-    //Add the parts together to make the new word
-    let newWord = capitalLetter + lowercaseRestOfWord;
-    //while the input is not Rock, Paper or Scissors keept asking, this loop keeps
-    //running until we get Rock, Paper or Scissors
-    while(newWord !== 'Rock' && newWord !== 'Paper' && newWord !== 'Scissors'){
-        alert('Please enter Rock, Paper or Scissors');
-        playerSelect = prompt('Rock, Paper or Scissors?');
-        //if the user didn't input anything this will alert the user and ask them again
-        while(playerSelect == null){
-            alert('Please enter Rock, Paper or Scissors');
-            playerSelect = prompt('Rock, Paper or Scissors?');
-        }
-        //Convert everything but the first letter the user inputted to lowercase
-        lowercaseRestOfWord = playerSelect.slice(1).toLowerCase();
-        //Convert the first letter the user inputted to uppercase
-        capitalLetter = playerSelect[0].toUpperCase();
-        //Add the parts together to make the new word
-        newWord = capitalLetter + lowercaseRestOfWord;
-    }
-    return newWord;
-}
+//Select the buttons and store in a variable
+const rockButton = document.querySelector('.Rock');
+const paperButton = document.querySelector('.Paper');
+const scissorsButton = document.querySelector('.Scissors');
+
+//Get the result of computerSelection
+let computerSelection = computerPlay();
+
+//Play 1 round by clicking on the button
+rockButton.addEventListener('click', (e)=> { 
+        playRound(e.target.classList.value, computerSelection)
+    });
+paperButton.addEventListener('click', (e)=> { 
+        playRound(e.target.classList.value, computerSelection)
+    });
+scissorsButton.addEventListener('click', (e)=> { 
+        playRound(e.target.classList.value, computerSelection)
+    });
+
 
 //This function is here to get the computer's selection, this function
 //randomly picks Rock, Paper or Scissors
@@ -52,59 +38,59 @@ function computerPlay(){
  }
 
 //Call the game function to start 5 rounds
-game();
+// game();
 
-function game(){
-    //Annouce to user in the console it is a 5 round game
-    console.log('Best of 5 wins');
-    //loop the game for 5 rounds
-    for (let i = 0; i < 5; i++){
-        //get the player selection and store in the playerSelection variable
-        let playerSelection = playerPlay();
-        //get the computer selection and store in the computerSelection variable
-        let computerSelection = computerPlay();
-        //play 1 round of the game with the selections as arguements
-        playRound(playerSelection, computerSelection);
-        //if the player score is 3 then the player has won best of 5 rounds
-        if(playerScore == 3){
-            console.log('Player wins');
-            //reset the scores to 0 when a winner has been found so the game function
-            //can be called again without having to refresh the page
-            playerScore = 0;
-            computerScore = 0;
-            //Return to exit the loop so it doesn't do the full 5 rounds if someone
-            //has won
-            return
-        }
-        //if the computer score is 3 then the player has won best of 5 rounds
-        if(computerScore == 3){
-            console.log('Computer wins');
-            //Reset the scores to 0 when a winner has been found so the game function
-            //can be called again without having to refresh the page
-            playerScore = 0;
-            computerScore = 0;
-            //Return to exit the loop so it doesn't do the full 5 rounds if someone
-            //has won
-            return
-        }
-     }
-     //if 5 rounds have been played and no one has 3 points then annouce a winner based
-     //on who has the most points.
+// function game(){
+//     //Annouce to user in the console it is a 5 round game
+//     console.log('Best of 5 wins');
+//     //loop the game for 5 rounds
+//     for (let i = 0; i < 5; i++){
+//         //get the player selection and store in the playerSelection variable
+//         let playerSelection = playerPlay();
+//         //get the computer selection and store in the computerSelection variable
+//         let computerSelection = computerPlay();
+//         //play 1 round of the game with the selections as arguements
+//         playRound(playerSelection, computerSelection);
+//         //if the player score is 3 then the player has won best of 5 rounds
+//         if(playerScore == 3){
+//             console.log('Player wins');
+//             //reset the scores to 0 when a winner has been found so the game function
+//             //can be called again without having to refresh the page
+//             playerScore = 0;
+//             computerScore = 0;
+//             //Return to exit the loop so it doesn't do the full 5 rounds if someone
+//             //has won
+//             return
+//         }
+//         //if the computer score is 3 then the player has won best of 5 rounds
+//         if(computerScore == 3){
+//             console.log('Computer wins');
+//             //Reset the scores to 0 when a winner has been found so the game function
+//             //can be called again without having to refresh the page
+//             playerScore = 0;
+//             computerScore = 0;
+//             //Return to exit the loop so it doesn't do the full 5 rounds if someone
+//             //has won
+//             return
+//         }
+//      }
+//      //if 5 rounds have been played and no one has 3 points then annouce a winner based
+//      //on who has the most points.
 
-     if(playerScore > computerScore){
-        console.log('Player wins');
-     }
-     else if(computerScore > playerScore){
-        console.log('Computer wins');
-     }
-     else{
-        console.log('No one wins best of 5 :(');
-     }
-     //Reset the scores to 0 after 5 rounds so the game function can be called again
-     //without having to refresh the page
-     playerScore = 0;
-     computerScore = 0;
-}
+//      if(playerScore > computerScore){
+//         console.log('Player wins');
+//      }
+//      else if(computerScore > playerScore){
+//         console.log('Computer wins');
+//      }
+//      else{
+//         console.log('No one wins best of 5 :(');
+//      }
+//      //Reset the scores to 0 after 5 rounds so the game function can be called again
+//      //without having to refresh the page
+//      playerScore = 0;
+//      computerScore = 0;
+// }
 
  function playRound(playerSelection, computerSelection){
      if(playerSelection == 'Rock' && computerSelection == 'Scissors'){
